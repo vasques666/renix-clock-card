c0a4423e7d796bdf2c0b38c017dc1724befd8c53
vasques666 <81989940+vasques666@users.noreply.github.com>
2026-08-24 12:40:46 +0300

    Update renix-clock-card.js

--- renix-clock-card.js
+++ renix-clock-card.js
@@ -1488,17 +1488,31 @@ const ampm=
         :
         '';
 
-    const top=
-      C.show_date
-        ?
-          `<div class="renix-header">
-            <div class="renix-date">
-              ${topIcon}
-              <span>${date}${topWeekday}</span>
-            </div>
-          </div>`
-        :
-        '';
+const top=
+  C.show_date
+    ?
+      `<div class="renix-header">
+        <div
+          class="
+            renix-date
+            ${C.weather_entity ? 'renix-clickable' : ''}
+          "
+          ${
+            C.weather_entity
+              ?
+                `data-entity-id="${this._escape(
+                  C.weather_entity
+                )}"`
+              :
+                ''
+          }
+        >
+          ${topIcon}
+          <span>${date}${topWeekday}</span>
+        </div>
+      </div>`
+    :
+      '';
 
     /* =====================================================
        SENSOR BLOCK
