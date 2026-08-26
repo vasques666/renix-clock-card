35fb43f22d0c219d0331b7090750e1a23bbb0be3
vasques666 <81989940+vasques666@users.noreply.github.com>
2026-08-26 13:41:01 +0300

    Update renix-clock-card.js

--- renix-clock-card.js
+++ renix-clock-card.js
@@ -209,7 +209,7 @@ const RENIX_CSS = `
 .renix-grid{
   position:absolute;top:0;height:100%;display:flex;align-items:flex-end;
   font-family:renix1,monospace;
-  font-size:calc(17rem * var(--renix-scale) * var(--renix-clock-factor));font-weight:400;
+  font-size:calc(17rem * var(--renix-scale) * var(--renix-clock-factor));font-weight:800;
   font-style:normal;line-height:.95;letter-spacing:-.12em;white-space:nowrap;
   pointer-events:none;z-index:5;color:transparent;
   background:repeating-linear-gradient( 0deg, rgba(0,0,0,0) 0, rgba(0,0,0,0) 5px, rgba(0,0,0,.9) 5px, rgba(0,0,0,.4) 6px ), repeating-linear-gradient( 90deg, rgba(0,0,0,0) 0, rgba(0,0,0,0) 5px, rgba(0,0,0,.9) 5px, rgba(0,0,0,.4) 6px );
